2287ebd72d1d8f2966d20d3198c90ab28b851596
Kaio Benevenuto Hoffman <kaiohoffmanbeneve@gmail.com>
2024-08-21 17:36:02 -0300

    add um evento e coloquei transição ao excluir uma lista

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 document.getElementById("newTaskList").addEventListener("click", function () {
-  document.getElementById("janelaDeTitulo").style.display = "block"
+  document.getElementById("janelaDeTitulo").style.display = "flex"
+  document.getElementById("title").focus()
 })
 
 document.getElementById("cancel").addEventListener("click", function () {
   document.getElementById("janelaDeTitulo").style.display = "none"
 })
 
-document.getElementById("confirm").addEventListener("click", function () {
-  let titulo = document.getElementById("title");
-  let tituloDaTaskList = titulo.value;
-  titulo.value = '';
-  document.getElementById("janelaDeTitulo").style.display = "none";
-  if(tituloDaTaskList !== ""){
+//Fazer com que quando o título for muito grande pra caber no espaço jogue uma parte dele para baixo 
+function returnTitle(){
+  let titulo = document.getElementById("title")
+  let tituloDaTaskList = titulo.value
+  titulo.value = ""
+  document.getElementById("janelaDeTitulo").style.display = "none"
+  return tituloDaTaskList
+}
+function addList(tituloDaTaskList){
+  if (tituloDaTaskList !== "") {
     const newList = document.createElement("div")
     newList.style.width = "100%"
     newList.style.height = "80px"
     newList.style.backgroundColor = "#606d80"
     newList.style.borderRadius = "10px"
-    newList.style.marginBottom = "6px"
+    newList.style.marginBottom = "8px"
     newList.style.marginTop = "4px"
     newList.style.display = "flex"
 
@@ -31,7 +36,6 @@ document.getElementById("confirm").addEventListener("click", function () {
     title.style.fontSize = "22px"
     title.style.fontFamily = "Rubik"
     title.style.alignItems = "center"
-    //Tenho que colocar um limite de caracteres ☝️
 
     let del = document.createElement("button")
     del.style.margin = "25px 25px"
@@ -54,18 +58,23 @@ document.getElementById("confirm").addEventListener("click", function () {
       del.style.transform = "scale(0.80)"
       setTimeout(function () {
         del.style.transform = "scale(1)"
-        
         newList.classList.add("fadeOut")
         setTimeout(function () {
           newList.remove()
         }, 400)
       }, 200)
-
-     
     })
 
-
-
     document.getElementById("container").appendChild(newList)
+  }
+}
+
+document.getElementById("confirm").addEventListener("click", function () {
+  addList(returnTitle())
+})
+
+document.addEventListener("keydown", function (event) {
+  if(event.key === 'Enter'){
+  document.getElementById("confirm").click()
   }
 })
