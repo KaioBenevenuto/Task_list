c4d874732ccd552cdd63b08ccca7684789fc9dec
Kaio Benevenuto Hoffman <kaiohoffmanbeneve@gmail.com>
2024-09-16 17:02:27 -0300

    melhorei as mecânicas da lista e troquei alguns ícones

--- Lista/script.js
+++ Lista/script.js
@@ -1,13 +1,13 @@
-let h1 = document.getElementById('listTitle')
+let h1 = document.getElementById("listTitle")
 let currentDivData = JSON.parse(localStorage.getItem("currentDiv"))
-
 
 if (currentDivData && currentDivData.title) {
   h1.textContent = currentDivData.title
 }
 
-document.querySelector("#checkbox").addEventListener("change", function () {
-  const line = document.getElementById("line")
+//Faz o risco no texto
+document.querySelector(".checkbox").addEventListener("change", function () {
+  const line = document.querySelector(".line").querySelector('.text')
 
   if (this.checked) {
     line.style.textDecoration = "line-through"
@@ -18,107 +18,122 @@ document.querySelector("#checkbox").addEventListener("change", function () {
   }
 })
 
-function habilitarEdicao(line) {
-  const inputTexto = line.getElementById("text")
-  const editarBtn = line.querySelector("#editarBtn")
+//Coloca o ícone nos buttons
+function btnImg(button) {
+  const altXlarg = "20px"
+  let img = document.createElement("img")
 
+  if (button.classList.contains("editarBtn")) {
+    img.src = "../assets/pencil.png"
+  } else if (button.classList.contains("adicionarBtn")) {
+    img.src = "../assets/adicionar.png"
+  } else if (button.classList.contains("removerBtn")) {
+    img.src = "../assets/cancelar.png"
+  }
+
+  img.style.width = altXlarg
+  img.style.height = altXlarg
+  button.appendChild(img)
+}
+
+//Habilita a edição do texto da linha
+function habilitarEdicao(event) {
+  const line = event.target.closest(".line")
+  const inputTexto = line.querySelector(".text")
+  const editarBtn = line.querySelector(".editarBtn")
+
+  inputTexto.disabled = false
   editarBtn.style.transform = "scale(0.80)"
   setTimeout(function () {
     editarBtn.style.transform = "scale(1)"
-    inputTexto.disabled = false
     inputTexto.focus()
     inputTexto.select()
   }, 100)
 }
 
-//imagens dos botões
-function btnImg(boolean){
-  const editarBtn = document.getElementById("editarBtn");
-  let imgEdit = document.createElement("img");
-  imgEdit.src = "../assets/pencil.png";
-  imgEdit.style.width = "20px";
-  imgEdit.style.height = "20px";
-  editarBtn.appendChild(imgEdit);
-
-  const adicionarBtn = document.getElementById("adicionarBtn")
-  let imgAdd = document.createElement("img")
-  imgAdd.src = "../assets/square.png"
-  imgAdd.style.width = "20px"
-  imgAdd.style.height = "20px"
-  adicionarBtn.appendChild(imgAdd)
-  
-  let newLine = boolean
-  if(newLine){
-    const removerBtn = document.getElementById("removerBtn")
-    let imgDelete = document.createElement("img")
-    imgDelete.src = "../assets/delete.png"
-    imgDelete.style.width = "20px"
-    imgDelete.style.height = "20px"
-    removerBtn.appendChild(imgDelete)
-  }
-}
-
-document.getElementById("text").addEventListener("blur", function () {
-  text.disabled = true
-})
-
-function adicionarLinha() {
-  const novaLinha = document.createElement('div');
-  novaLinha.classList.add('line');
-
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.classList.add('checkbox');
-
-  const inputTexto = document.createElement('input');
-  inputTexto.type = 'text';
-  inputTexto.classList.add('text');
-  inputTexto.disabled = true;
-
-  const editarBtn = document.createElement('button');
-  editarBtn.classList.add('editarBtn');
-  editarBtn.addEventListener('click', function () {
-    habilitarEdicao(novaLinha);
-  });
-
-  const removerBtn = document.createElement('button');
-  //removerBtn.textContent = 'Remover';
-  removerBtn.classList.add('removerBtn');
-  removerBtn.style.display = 'none';
-  removerBtn.addEventListener('click', function () {
-    novaLinha.remove();
-  });
-
-  const adicionarBtn = document.createElement('button');
-  //adicionarBtn.textContent = 'Adicionar';
-  adicionarBtn.classList.add('adicionarBtn');
-  adicionarBtn.addEventListener('click', adicionarLinha);
-
-  novaLinha.appendChild(checkbox);
-  novaLinha.appendChild(inputTexto);
-  novaLinha.appendChild(editarBtn);
-  novaLinha.appendChild(removerBtn);
-  novaLinha.appendChild(adicionarBtn);
-
-  // Somente mostrar o botão remover ao passar o mouse
-  novaLinha.addEventListener('mouseover', function () {
-    if (novaLinha !== document.querySelector('.listaContainer .line:last-child')) {
-      removerBtn.style.display = 'inline-block';
+//Desabilita a edição
+document.querySelector(".listaContainer").addEventListener(
+  "blur",
+  function (event) {
+    if (event.target.classList.contains("text")) {
+      event.target.disabled = true
     }
-  });
+  },
+  true
+)
+
+//adiciona uma linha nova
+function adicionarLinha() {
+  const novaLinha = document.createElement("div")
+  novaLinha.classList.add("line")
+
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.classList.add("checkbox")
+  checkbox.addEventListener("change", function () {
+    const line = this.closest(".line").querySelector(".text")
+    if (this.checked) {
+      line.style.textDecoration = "line-through"
+      line.style.color = "rgba(138, 138, 138, 0.522)"
+    } else {
+      line.style.textDecoration = "none"
+      line.style.color = "black"
+    }
+  })
+
+  const inputTexto = document.createElement("input")
+  inputTexto.type = "text"
+  inputTexto.classList.add("text")
+  inputTexto.disabled = true
+
+  const editarBtn = document.createElement("button")
+  editarBtn.classList.add("editarBtn")
+  editarBtn.addEventListener("click", habilitarEdicao)
+  btnImg(editarBtn) // Adiciona a imagem do botão
+
+  const removerBtn = document.createElement("button")
+  removerBtn.classList.add("removerBtn")
+  removerBtn.style.display = "none"
+  removerBtn.addEventListener("click", function () {
+    novaLinha.remove()
+  })
+  btnImg(removerBtn) // Adiciona a imagem do botão
+
+  const adicionarBtn = document.createElement("button")
+  adicionarBtn.classList.add("adicionarBtn")
+  adicionarBtn.addEventListener("click", adicionarLinha)
+  btnImg(adicionarBtn) // Adiciona a imagem do botão
+
+  novaLinha.appendChild(checkbox)
+  novaLinha.appendChild(inputTexto)
+  novaLinha.appendChild(editarBtn)
+  novaLinha.appendChild(adicionarBtn)
+  novaLinha.appendChild(removerBtn)
+
+  // Mostrar o botão remover ao passar o mouse
+  novaLinha.addEventListener("mouseover", function () {
+    if (
+      novaLinha !== document.querySelector(".listaContainer .line") //Estava antes ".line:last-child"
+    ) {
+      removerBtn.style.display = "inline-block"
+    }
+  })
 
   novaLinha.addEventListener("mouseout", function () {
     removerBtn.style.display = "none"
   })
 
-  btnImg(true)
-  document.getElementById("listaContainer").appendChild(novaLinha)
+  document.querySelector(".listaContainer").appendChild(novaLinha)
 }
 
-document.querySelector('#adicionarBtn').addEventListener('click', adicionarLinha);
-
-document.querySelector("#editarBtn").addEventListener("click", function () {
-  habilitarEdicao(document.querySelector(".line"))
+// Inicia ícones e eventos na primeira linha
+document.querySelectorAll('.line').forEach(line => {
+  line.querySelectorAll('button').forEach(button => btnImg(button))
+  line.querySelector('.editarBtn').addEventListener('click', habilitarEdicao)
+  line.querySelector('.adicionarBtn').addEventListener('click', adicionarLinha)
 })
 
-btnImg(false)
+
+document.querySelector('.editarBtn').addEventListener('click', habilitarEdicao)
+document.querySelector(".adicionarBtn").addEventListener("click", adicionarLinha)
+//Tenho que tirar a linha que eu crie no HTML sem apagar as classes
